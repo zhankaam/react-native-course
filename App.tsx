bc833393041461823b8cc5ffd238fb9aba4db4ca
Zhanat Lepesbayeva <zhanat.lepesbaeva@effective-soft.com>
2023-02-13 21:47:26 +0300

    init app

---
+++ App.tsx
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, Text, useColorScheme} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <Text>Hello World!</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
