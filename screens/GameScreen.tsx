e3f2b94f58e50935d8b08db0d6f6290d08bc6738
Zhanat Lepesbayeva <zhanat.lepesbaeva@effective-soft.com>
2023-02-23 07:32:04 +0300

    016: starting work on the game screen

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 function GameScreen() {
-  return <Text>GameScreen</Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      {/* GUESS */}
+      <View>
+        <Text> Higher or lower?</Text>
+        {/* + - */}
+      </View>
+      <View>{/* LOG ROUNDS */}</View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
 
 export default GameScreen;
