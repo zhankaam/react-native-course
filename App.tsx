81f83e725ad3ad61b5c9692897d3385614ebf05b
Zhanat Lepesbayeva <zhanat.lepesbaeva@effective-soft.com>
2023-02-23 05:23:01 +0300

    010: coloring the components & overall app

--- App.tsx
+++ App.tsx
@@ -6,10 +6,22 @@
  */
 
 import React from 'react';
+import {StyleSheet, View} from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 
 function App(): JSX.Element {
-  return <StartGameScreen />;
+  return (
+    <View style={styles.rootScreen}>
+      <StartGameScreen />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+    backgroundColor: '#ddb52f',
+  },
+});
 
 export default App;
