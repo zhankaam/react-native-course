3faba7fca6754a9155974d0a51d3fb63fb15b198
Zhanat Lepesbayeva <zhanat.lepesbaeva@effective-soft.com>
2023-02-25 18:46:27 +0300

    027: adding a foreground image

--- screens/GameOverScreen.tsx
+++ screens/GameOverScreen.tsx
@@ -1,8 +1,43 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Image, StyleSheet, View, Text} from 'react-native';
+import Title from '../components/ui/Title';
+import {COLORS} from '../constants/colors';
 
 function GameOverScreen() {
-  return <Text>Game is over!</Text>;
+  return (
+    <View style={styles.rootContainer}>
+      <Title>GAME OVER!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require('../assets/images/success.png')}
+          style={styles.image}
+        />
+      </View>
+      <Text>Your phone needed X rounds to guess the number Y.</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: COLORS.primary800,
+    overflow: 'hidden',
+    margin: 36,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default GameOverScreen;
