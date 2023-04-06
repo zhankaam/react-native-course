9328af6ad06c636856cfbb1803575704fa70d59b
Zhanat Lepesbayeva <zhanat.lepesbaeva@effective-soft.com>
2023-04-07 01:01:55 +0300

    005: getting started with the react navigation package

--- App.tsx
+++ App.tsx
@@ -8,12 +8,20 @@
 import React from 'react';
 import CategoriesScreen from './screens/CategoriesScreen';
 import {StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <CategoriesScreen />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
