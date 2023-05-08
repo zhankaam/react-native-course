ed81fedc36ee3df9ab06aee1e0fa507c5d790ca6
Zhanat Lepesbayeva <zhanat.lepesbaeva@effective-soft.com>
2023-05-09 02:37:24 +0300

    019: Adding Drawer Navigation & Creating a Drawer

---
+++ screens/UserScreen.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export const UserScreen = () => {
+  return (
+    <View>
+      <Text>UserScreen</Text>
+    </View>
+  );
+};
---
+++ screens/WelcomeScreen.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export const WelcomeScreen = () => {
+  return (
+    <View>
+      <Text>WelcomeScreen</Text>
+    </View>
+  );
+};
