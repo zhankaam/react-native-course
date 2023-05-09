5a3b1dd2056cfb0ad7f86a35558dd76254a003a6
Zhanat Lepesbayeva <zhanat.lepesbaeva@effective-soft.com>
2023-05-09 03:31:57 +0300

    020: configuring the drawer navigator & the drawer

--- App.tsx
+++ App.tsx
@@ -10,15 +10,47 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {WelcomeScreen} from './screens/WelcomeScreen';
 import {UserScreen} from './screens/UserScreen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+Ionicons.loadFont();
 
 const Drawer = createDrawerNavigator();
 
 function App(): JSX.Element {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="User">
-        <Drawer.Screen name="Welcome" component={WelcomeScreen} />
-        <Drawer.Screen name="User" component={UserScreen} />
+      <Drawer.Navigator
+        initialRouteName="User"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#3c0a6b',
+          },
+          headerTintColor: 'white',
+          drawerActiveBackgroundColor: '#f0e1ff',
+          drawerActiveTintColor: '#3c0a6b',
+          // drawerStyle: {
+          //   backgroundColor: '#ccc',
+          // },
+        }}>
+        <Drawer.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{
+            drawerLabel: 'Welcome Screen',
+            drawerIcon: ({color, size}) => (
+              <Ionicons name="home" size={30} color={color} size={size} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="User"
+          component={UserScreen}
+          options={{
+            drawerIcon: ({color, size}) => (
+              <Ionicons name="person" size={30} color={color} size={size} />
+            ),
+          }}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
--- screens/UserScreen.tsx
+++ screens/UserScreen.tsx
@@ -1,10 +1,34 @@
-import {View, Text} from 'react-native';
+import {View, Text, Button, StyleSheet} from 'react-native';
 import React from 'react';
 
-export const UserScreen = () => {
+type PropsType = {
+  navigation: any;
+};
+
+export const UserScreen = ({navigation}: PropsType) => {
+  function openDrawerHandler() {
+    navigation.toggleDrawer();
+  }
+
   return (
-    <View>
-      <Text>UserScreen</Text>
+    <View style={styles.rootContainer}>
+      <Text>
+        This is the <Text style={styles.highlight}>User </Text>
+        screen!
+      </Text>
+      <Button title="Open Drawer" onPress={openDrawerHandler} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  highlight: {
+    fontWeight: 'bold',
+    color: '#eb1064',
+  },
+});
