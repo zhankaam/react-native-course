5a075f51b394e530f4a90ddb25cb6861fcc408ff
Zhanat Lepesbayeva <zhanat.lepesbaeva@effective-soft.com>
2023-06-14 00:41:53 +0300

    017: adding custom buttons

---
+++ UI/Button.tsx
@@ -0,0 +1,57 @@
+import {
+  Pressable,
+  StyleProp,
+  StyleSheet,
+  Text,
+  View,
+  ViewStyle,
+} from 'react-native';
+import React from 'react';
+import {GlobalStyles} from '../constants/styles';
+
+type PropsType = {
+  children: string;
+  mode?: string;
+  onPress: () => void;
+  style?: StyleProp<ViewStyle>;
+};
+
+const Button: React.FC<PropsType> = ({children, onPress, mode, style}) => {
+  return (
+    <View style={style}>
+      <Pressable
+        onPress={onPress}
+        style={({pressed}) => pressed && styles.pressed}>
+        <View style={[styles.button, mode === 'flat' && styles.flat]}>
+          <Text style={[styles.buttonText, mode === 'flat' && styles.flatText]}>
+            {children}
+          </Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
+
+export default Button;
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 4,
+    padding: 8,
+    backgroundColor: GlobalStyles.colors.primary500,
+  },
+  flat: {
+    backgroundColor: 'transparent',
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  flatText: {
+    color: GlobalStyles.colors.primary200,
+  },
+  pressed: {
+    opacity: 0.75,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+});
--- screens/ManageExpense.tsx
+++ screens/ManageExpense.tsx
@@ -4,6 +4,7 @@ import {StyleSheet, View} from 'react-native';
 import {StackParamList} from '../App';
 import IconButton from '../UI/IconButton';
 import {GlobalStyles} from '../constants/styles';
+import Button from '../UI/Button';
 
 export function ManageExpense({
   route,
@@ -19,9 +20,17 @@ export function ManageExpense({
   }, [isEditing, navigation]);
 
   function deleteExpenseHandler() {}
+  function cancelHandler() {}
+  function confirmHandler() {}
 
   return (
     <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button onPress={confirmHandler}>{isEditing ? 'Update' : 'Add'}</Button>
+      </View>
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconButton
@@ -41,6 +50,15 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
   deleteContainer: {
     marginTop: 16,
