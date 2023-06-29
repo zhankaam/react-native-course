57e76e6b36ba68738d1d2f5f59a7a99028345e11
Zhanat Lepesbayeva <zhanat.lepesbaeva@effective-soft.com>
2023-06-30 01:48:54 +0300

    005: adding styling

--- components/ManageExpense/Input.tsx
+++ components/ManageExpense/Input.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
-import {Text, TextInput, TextInputProps, View} from 'react-native';
+import {
+  StyleProp,
+  StyleSheet,
+  Text,
+  TextInput,
+  TextInputProps,
+  TextStyle,
+  View,
+} from 'react-native';
+import {GlobalStyles} from '../../constants/styles';
 
 interface Props {
   label: string;
@@ -7,12 +16,41 @@ interface Props {
 }
 
 function Input({label, textInputConfig}: Props) {
+  const inputStyles = [styles.input] as Array<StyleProp<TextStyle>>;
+
+  if (textInputConfig && textInputConfig.multiline) {
+    inputStyles.push(styles.inputMultiline);
+  }
+
   return (
-    <View>
-      <Text>{label}</Text>
-      <TextInput {...textInputConfig} />
+    <View style={styles.inputContainer}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput style={styles.input} {...textInputConfig} />
     </View>
   );
 }
 
 export default Input;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 8,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.primary100,
+    color: GlobalStyles.colors.primary700,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: 'top',
+  },
+});
