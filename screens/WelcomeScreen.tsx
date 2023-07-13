24233872dc3b46a07fc1211be4e0c08ed3d62a9e
Zhanat Lepesbayeva <zhanat.lepesbaeva@effective-soft.com>
2023-07-13 14:03:43 +0300

    014: Accessing Protected Resources

--- screens/WelcomeScreen.tsx
+++ screens/WelcomeScreen.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useContext, useEffect, useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {AuthContext} from '../store/auth-context';
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState('');
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://react-native-course-6a569-default-rtdb.firebaseio.com/message.json?auth=' +
+          token,
+      )
+      .then(response => {
+        setFetchedMessage(response.data);
+      });
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
