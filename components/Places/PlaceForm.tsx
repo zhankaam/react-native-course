24cdb13b916ae0acc0e56f0cc24c845c03c4bf8d
Zhanat Lepesbayeva <zhanat.lepesbaeva@effective-soft.com>
2023-07-24 00:03:52 +0300

    007: getting started with a custom form

--- components/Places/PlaceForm.tsx
+++ components/Places/PlaceForm.tsx
@@ -1,12 +1,47 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, ScrollView, TextInput, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {Colors} from '../../constants/colors';
 
 const PlaceForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+
+  function changeTitleHandler(enteredText: string) {
+    setEnteredTitle(enteredText);
+  }
+
   return (
-    <View>
-      <Text>PlaceForm</Text>
-    </View>
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.input}
+          value={enteredTitle}
+          onChangeText={changeTitleHandler}
+        />
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: 'bold',
+    marginBottom: 4,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100,
+  },
+});
 
 export default PlaceForm;
